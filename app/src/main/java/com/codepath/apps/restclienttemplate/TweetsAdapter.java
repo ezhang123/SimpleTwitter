dab9f7450035d3c970d5d9debff43829a61120ca
ezhang123 <76235043+ezhang123@users.noreply.github.com>
2022-02-14 21:35:27 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,9 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.concurrent.RecursiveAction;
+
+public class TweetsAdapter {
+    
+}
